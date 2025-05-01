59ce952a38e8d1ade1270769565ea2a8a4ca8bef
toikaz <zxcursech@gmail.com>
2025-05-01 20:03:23 +0300

    Uninstall.bat

--- bin/Uninstall.bat
+++ bin/Uninstall.bat
@@ -1,15 +1,10 @@
 @echo off
 echo Uninstalling HPP...
 timeout 2 >nul
-set /p VAR1=<C:\hpp.uninstall >nul
-set /? |find "/P" >nul
-set /p VAR2=<C:\hpp_ide.uninstall >nul
-set /? |find "/P" >nul
-set /p VAR3=<C:\vvm.uninstall >nul
-set /? |find "/P" >nul
-del %var1% >nul
-del %var2% >nul
-del %var3% >nul
+del C:\Windows\System32\vvm.exe >nul
+del C:\Windows\System32\hpp.exe >nul
+del C:\users\%username\desktop\hpp_ide.exe >nul
+del C:\Windows\System32\hppinstaller.exe.exe >nul
 del C:\vvm.uninstall >nul
 del C:\hpp.uninstall >nul
 del C:\hpp_ide.uninstall >nul
