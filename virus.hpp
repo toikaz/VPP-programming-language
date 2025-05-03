c8c62b8a26fa3ea19ed4ca61e81f7af1d66885d3
toikaz <zxcursech@gmail.com>
2025-05-03 14:19:19 +0300

    mini virus

---
+++ virus.hpp
@@ -0,0 +1,14 @@
+public static void Main(String[] args) {
+
+    System.process.kill explorer.exe
+    System.create.window Virus 500 200
+    System.add_text.window Virus 150 10 you have a virus!
+    System.add_text.window Virus 150 25 Enter a password
+    System.get.window Virus pass 50 20 10 10
+    str a = "shutdown /s"
+    int p = 2332
+    if pass != %p% : goto 1
+    if pass == %p% : System.start explorer
+    goto 1
+    
+}
