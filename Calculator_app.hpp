63ffe9639ed94af29590295d61ced0e5d5bf99ca
toikaz <zxcursech@gmail.com>
2025-05-03 07:32:32 +0300

    Calculator app

--- Calculator_app.hpp
+++ Calculator_app.hpp
@@ -1,12 +1,12 @@
 public static void Main(String[] args) {
 
-    System.create.window Calculator 500 500
-    System.add_text.window Calculator Enter first number:
-    System.get.window Calculator fn
-    System.add_text.window Calculator Enter second number:
-    System.get.window Calculator sn
-    System.add_text.window Calculator Enter sing + - * /:
-    System.get.window Calculator si
+    System.create.window Calculator 200 200
+    System.add_text.window Calculator 10 10 Enter first number:
+    System.get.window Calculator fn 60 20 20 30
+    System.add_text.window Calculator 10 45 Enter second number:
+    System.get.window Calculator sn 60 20 20 65
+    System.add_text.window Calculator 10 80 Enter sing + - * /:
+    System.get.window Calculator si 60 20 20 100
     str p = "+"
     str m = "-"
     str y = "*"
@@ -19,7 +19,7 @@ public static void Main(String[] args) {
     if %si% == %y% : System.out.print %result%
     if %si% == %r% : int result = %fn% / %sn%
     if %si% == %r% : System.out.print %result%
-    System.add_text.window Calculator Result:
-    System.add_text.window Calculator %result%
+    System.add_text.window Calculator 10 120 Result:
+    System.add_text.window Calculator 60 120 %result%
     
 }
